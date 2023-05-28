c853b2ad0c45bb9564fd4b7172d6536462b28379
Muhammad Irfa'issurur <fais.fais611@gmail.com>
2023-05-28 21:40:30 +0700

    Coba font

--- app/Views/coba.php
+++ app/Views/coba.php
@@ -6,12 +6,135 @@
         <div class="col">
             <h1 class="fw-bolder">Coba masing-masing tulis apapun di sini : </h1>
             <ul>
-                <li><h2>Hai, ini Shawna</h2></li>
+                <li>
+                    <h2>Hai, ini Shawna</h2>
+                </li>
+                <li>
+                    <h2>Hai, ini Fais</h2>
+                </li>
                 <!-- TULIS DI SINI -->
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="mt-5 container col-sm-8">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <ol class="mt-3">
+                    <h4 style="font-weight:bolder;">Daftar Task (Default)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Roboto', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Roboto)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Open Sans', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Open Sans)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Lato', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Lato)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Poppins', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Poppins)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Nunito', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Nunito)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
+
+                <ol class="mt-3" style="font-family: 'Montserrat', sans-serif;">
+                    <h4 style="font-weight:bolder;">Daftar Task (Montserrat)</h4>
+                    <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                    <ul>
+                        <li>Tulis sesuatu di file coba.php (lihat comment di codingannya)</li>
+                        <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                        <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                    </ul>
+                    <li>Berikan pendapat tentang style navbar dan footer (memang belum responsif)</li>
+                    <li>Lihat jenis font yang ditampilkan, font mana yang bagus menurut kalian?</li>
+                </ol>
             </ul>
         </div>
     </div>
-    
+
     <div class="row mt-5 pt-5">
         <div class="col">
             <h1 class="fw-bolder">Jenis Fonts</h1>
@@ -24,27 +147,27 @@
                 <h2>Ini Font Roboto</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
             </div>
-            
+
             <div style="font-family: 'Lato', sans-serif;">
                 <h2>Ini Font Lato</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
             </div>
-            
+
             <div style="font-family: 'Open Sans', sans-serif;">
                 <h2>Ini Font Open Sans</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
             </div>
-            
+
             <div style="font-family: 'Poppins', sans-serif;">
                 <h2>Ini Font Poppins</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
             </div>
-            
+
             <div style="font-family: 'Nunito', sans-serif;">
                 <h2>Ini Font Nunito</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
             </div>
-           
+
             <div style="font-family: 'Montserrat', sans-serif;">
                 <h2>Ini Font Montserrat</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis libero harum similique quisquam eos aperiam, eaque quos aspernatur, tempora totam soluta commodi inventore amet distinctio illum modi animi esse!</p>
