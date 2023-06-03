f0757a04a793da42946040209e945e33487036c5
ShawnaClaire <claireees88@gmail.com>
2023-06-03 13:21:57 +0700

    menyelesaikan navbar-v2

---
+++ app/Views/layout/navbar-v2.php
@@ -0,0 +1,94 @@
+<!-- Top Header -->
+<div class="container-fluid bg-darkorange">
+  <div class="container-md">
+    <div class="row">
+      <div class="col-md-7 rounded-pill d-none d-md-flex align-items-center bg-lightorange2 justify-content-between">
+        <a class="text-decoration-none text-darkbrown" style="font-weight: 900;" href="#">
+          <img src="/assets/images/logo-bps.svg" alt="Logo-BPS" style="max-width: 18%; padding: 6px;" class="d-inline-block align-text-center">
+          <i>
+            BADAN PUSAT STATISTIK
+          </i>
+        </a>
+        
+          <img src="/assets/images/logo-se2016.png" alt="Logo-Sensus-Ekonomi-2016" style="max-width: 20%;" class="align-text-center pe-2">
+        
+      </div>
+
+    </div>
+
+  </div>
+</div>
+<!-- End of Top Header -->
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-dark navbar-expand-lg bg-body-tertiary mb-0 ps-4 pe-4 pt-1 pb-2 bg-darkorange sticky-top">
+  <div class="container-md">
+    <div class="row">
+      <div class="col-6 col-sm-6 d-flex d-md-none justify-content-start align-items-center p-0">
+        <img src="/assets/images/logo-se2016.png" alt="Logo-Sensus-Ekonomi-2016" style="max-width: 75%; max-height: 75%;" class="align-text-center bg-lightorange2 rounded-pill ps-3 pe-3">
+      </div>
+      <div class="col-6 col-sm-6 col-md-12 d-flex justify-content-end">
+        <button class="navbar-toggler ms-auto border border-3 border-lightorange" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon "></span>
+        </button>
+      </div>
+    </div>
+
+    <div class="collapse navbar-collapse nav-custom" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-1">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">BERANDA</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            LAYANAN DATA
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Tabel Topik</a></li>
+            <li><a class="dropdown-item" href="#">Perbandingan Antar Wilayah</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">SE2016 LANJUTAN</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PRODUK STATISTIK
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Publikasi</a></li>
+            <li><a class="dropdown-item" href="#">Infografis</a></li>
+            <li><a class="dropdown-item" href="#">Dokumen</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            TENTANG SE2016
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Metadata</a></li>
+            <li><a class="dropdown-item" href="#">Media</a></li>
+            <li><a class="dropdown-item" href="#">Timeline</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 bg-lightorange border border-darkorange" type="search" placeholder="Cari informasi..." aria-label="Search">
+        <a class="btn btn-outline-light border border-3 border-lightorange text-bold" href="" aria-label="search"><i class="bi bi-search"></i></a>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- End of Navbar -->
+
+
+<!-- Tagline -->
+<div class="container-fluid bg-darkbrown mt-0 ps-5 pe-5 pt-2 pb-2 text-light d-md-flex">
+  <!-- <span class="badge text-bg-darkorange text-light p-2">Tagline</span> -->
+  <marquee>Mencatat Ekonomi Indonesia</marquee>
+</div>
