80623244b78e91de9fd4ea2fc747a69058501f16
Muhammad Irfa'issurur <fais.fais611@gmail.com>
2023-06-07 22:44:24 +0700

    Membuat Tentang SE 2016 | Metadata

---
+++ app/Views/tentang-se/metadata/met-variabel.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <!-- <h1>Hello, World!</h1> -->
+
+    <div class="search_wrap col-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="search_box">
+            <input type="text" class="search" placeholder="Cari...">
+            <a class="button" href="#"> Cari
+                <!-- <p class="margin_0" href="#">Cari</p> -->
+            </a>
+        </div>
+    </div>
+
+
+    <div class="row my-5" data-aos="fade-up" data-aos-delay="100">
+        <div class="col-2 col-sm-1"></div>
+        <div class="col-8 col-sm-10">
+            <div class="alfabet-nav my-4 py-3 px-5 d-flex align-content-around flex-wrap justify-content-center">
+                <a href="#">A</a> <span> | </span>
+                <a href="#">B</a> <span> | </span>
+                <a href="#">C</a> <span> | </span>
+                <a href="#">D</a> <span> | </span>
+                <a href="#">E</a> <span> | </span>
+                <a href="#">F</a> <span> | </span>
+                <a href="#">G</a> <span> | </span>
+                <a href="#">H</a> <span> | </span>
+                <a href="#">I</a> <span> | </span>
+                <a href="#">J</a> <span> | </span>
+                <a href="#">K</a> <span> | </span>
+                <a href="#">L</a> <span> | </span>
+                <a href="#">M</a> <span> | </span>
+                <a href="#">N</a> <span> | </span>
+                <a href="#">O</a> <span> | </span>
+                <a href="#">P</a> <span> | </span>
+                <a href="#">Q</a> <span> | </span>
+                <a href="#">R</a> <span> | </span>
+                <a href="#">S</a> <span> | </span>
+                <a href="#">T</a> <span> | </span>
+                <a href="#">U</a> <span> | </span>
+                <a href="#">V</a> <span> | </span>
+                <a href="#">W</a> <span> | </span>
+                <a href="#">X</a> <span> | </span>
+                <a href="#">Y</a> <span> | </span>
+                <a href="#">Z</a>
+            </div>
+        </div>
+        <div class="col-2 col-sm-1"></div>
+    </div>
+
+    <div class="table" data-aos="fade-up" data-aos-delay="100">
+        <table class="table table-striped">
+            <thead class="bg-orange">
+                <tr class="">
+                    <th scope="col top-left" style="padding: 15px 30px;border-top-left-radius: 13px;">Istilah</th>
+                    <th scope="col top-right" style="padding: 15px 30px; border-top-right-radius: 13px;">Deskripsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet</td>
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                </tr>
+                <tr class="">
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet</td>
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                </tr>
+                <tr class="">
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet</td>
+                    <td class="" style="padding: 15px 30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+
+
+
+
+</div>
