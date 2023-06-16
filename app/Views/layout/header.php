1039b473888c4d412ce6830723e50159009a4b20
salsaagz <222011592@stis.ac.id>
2023-06-16 08:32:35 +0700

    edit collection dokumen

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -51,6 +51,7 @@
     <link rel="stylesheet" href="/assets/css/main.css">
     <link rel="stylesheet" href="/assets/css/navbar.css">
     <link rel="stylesheet" href="/assets/css/produkStatistik.css">
+    <link rel="stylesheet" href="/assets/css/beranda.css">
 </head>
 
 <body>
--- app/Views/produk-statistik/dokumen/booklet.php
+++ app/Views/produk-statistik/dokumen/booklet.php
@@ -1,4 +1,5 @@
-<!-- filter -->
+<!-- FILTER -->
+<!-- CODING MANUAL -->
 <div class="container">
         <div class="content">
             <form>
@@ -37,8 +38,9 @@
         </div>
     </div>
 
-    <!-- dokumen -->
-    <div class="container" id="cards_landscape_wrap-2">
+    <!-- BOOKLET -->
+    <!-- TEMPLATE -->
+    <!-- <div class="container" id="cards_landscape_wrap-2">
         <div class="content gallery" id="gallery">
         <div class="row gy-4 justify-content-center">
                 <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
@@ -170,5 +172,40 @@
                 <button type="submit" class="btn btn-primary bottom-0 end-0">Lainnya</button>
             </div>
         </div>
+        </div> -->
+        
+
+       
+        <div class="container-fluid pt-5 pb-5 mt-4 bg-lightorange">
+        <div class="container-md">
+        <h3 class="text-center fw-700 pb-4">Booklet</h3>
+        <div class="col-12 d-flex flex-md-row justify-content-center align-items-center gap-4 flex-wrap">
+
+            <a class="w-80 sm:w-40 md:w-22" href="https://www.bps.go.id/publication/2019/03/29/686ec20b624d7ddedb92255a/ringkasan-eksekutif-hasil-pendataan-usaha-perusahaan-sensus-ekonomi-20" target="_blank" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="0">
+                <div class="card-publikasi-beranda shadow mt-2 mb-2 d-flex flex-column justify-content-end align-items-center" style="border-radius: 40px; background-image: url('/assets/images/cover-publikasi1.png'); background-size: cover; height: 50vh;">
+                    <div class="text-center bg-darkbrown text-white p-3 fw-800 text-sm" style="border-bottom-left-radius: 40px; border-bottom-right-radius: 40px;">Ringkasan Eksekutif Hasil Pendataan Usaha/Perusahaan Sensus Ekonomi 2016-Lanjutan Indonesia</div>
+                </div>
+            </a>
+
+            <a class="w-80 sm:w-40 md:w-22" href="https://www.bps.go.id/publication/2018/12/31/ca2523a255de747559ca6b31/hasil-pendataan-usaha-perusahaan-pertambangan-dan-penggalian-sensus-ekonomi-2016-lanjutan-indonesia.html" target="_blank" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="50">
+                <div class=" card-publikasi-beranda shadow mt-2 mb-2 d-flex flex-column justify-content-end align-items-center" style="border-radius: 40px; background-image: url('/assets/images/cover-publikasi2.png'); background-size: cover; height: 50vh;">
+                    <div class="text-center bg-darkbrown text-white p-3 fw-800 text-sm" style="border-bottom-left-radius: 40px; border-bottom-right-radius: 40px;">Hasil Pendataan Usaha/Perusahaan Pertambangan dan Penggalian SE 2016-Lanjutan Indonesia</div>
+                </div>
+            </a>
+
+            <a class="d-none d-sm-block w-80 sm:w-40 md:w-22" src="/assets/images/Booklet 1.png" target="_blank" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100">
+                <div class=" card-publikasi-beranda shadow mt-2 mb-2 d-flex flex-column justify-content-end align-items-center" style="border-radius: 40px; background-image: url('/assets/images/cover-publikasi3.png'); background-size: cover; height: 50vh;">
+                    <div class="text-center bg-darkbrown text-white p-3 fw-800 text-sm" style="border-bottom-left-radius: 40px; border-bottom-right-radius: 40px;">Hasil Pendataan Usaha/Perusahaan Aktivitas Kesehatan Manusia SE 2016-Lanjutan Indonesia</div>
+                </div>
+            </a>
+
+            <a class="d-none d-sm-block w-80 sm:w-40 md:w-22" href="https://www.bps.go.id/publication/2018/12/31/67e12fbb1b473ac0c8efeb1a/hasil-pendataan-usaha-perusahaan-konstruksi-sensus-ekonomi-2016-lanjutan-indonesia-.html" target="_blank" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="150">
+                <div class=" card-publikasi-beranda shadow mt-2 mb-2 d-flex flex-column justify-content-end align-items-center" style="border-radius: 40px; background-image: url('/assets/images/cover-publikasi4.png'); background-size: cover; height: 50vh;">
+                    <div class="text-center bg-darkbrown text-white p-3 fw-800 text-sm" style="border-bottom-left-radius: 40px; border-bottom-right-radius: 40px;">Hasil Pendataan Usaha/Perusahaan Konstruksi Sensus Ekonomi 2016-Lanjutan Indonesia</div>
+                </div>
+            </a>
+
         </div>
+        <div class="d-flex justify-content-center pb-4 pt-2">
+            <button class="btn btn-orange-carrot text-black hover:shadow-black mt-3 fw-700 w-80 sm:w-60 md:w-40 rounded-pill">Lihat Publikasi Lainnya</button> 
     </div>
