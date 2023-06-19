3bc3b4bd55236b6bd639f4cf4891ea23a0a63c62
ShawnaClaire <claireees88@gmail.com>
2023-06-19 12:44:09 +0700

    memperbaiki grafik di layanan-data tabel topik

--- app/Views/beranda.php
+++ app/Views/beranda.php
@@ -137,7 +137,7 @@
 
         <canvas id="myChart"></canvas>
     </div>
-    <script src="assets/js/chart-beranda.js"></script>
+    <script src="/assets/js/chart-beranda.js"></script>
 </div>
 
 <!-- PUBLIKASI -->
